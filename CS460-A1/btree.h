8184076c538e9a153eeee73a8cc5055714bd6191
Hannah <lormenyoh@gmail.com>
2019-10-22 10:23:13 +0000

    removed some comments

--- CS460-A1/btree.h
+++ CS460-A1/btree.h
@@ -1,15 +1,8 @@
-/*
+/***
+ * Author: Hannah Boadiwaaa Lormenyo
+ * Organization: Ashesi University
  * 
- * You will need to write your B+Tree almost entirely from scratch. 
- * 
- * B+Trees are dynamically balanced tree structures that provides efficient support for insertion, deletion, equality, and range searches. 
- * The internal nodes of the tree direct the search and the leaf nodes hold the base data..
- * 
- * For a basic rundown on B+Trees, we will refer to parts of Chapter 10 of the textbook Ramikrishnan-Gehrke 
- * (all chapters and page numbers in this assignment prompt refer to the 3rd edition of the textbook).
- *
- * Read Chapter 10 which is on Tree Indexing in general. In particular, focus on Chapter 10.3 on B+Tree.
- */
+***/
 
 #ifndef BTREE_H
 #define BTREE_H
@@ -20,14 +13,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 
-Designing your C Structs for B+Tree nodes (Chapter 10.3.1)
-How will you represent a B+Tree as a C Struct (or series of C structs that work together)? There are many valid ways to do this part of your design, and we leave it open to you to try and tune this as you progress through the project.
-How will you account for a B+Tree node being an internal node or a leaf node? Will you have a single node type that can conditionally be either of the two types, or will you have two distinct struct types?
-How many children does each internal node have? This is called the fanout of the B+Tree.
-What is the maximum size for a leaf node? How about for an internal node?
-What is the minimum threshold of content for a node, before it has to be part of a rebalancing?
-*/
+
+//INITIALIZERS/FUNCTION PROTOTYPES
+void insert(int key, int value);
+struct nodePosition find(int key);
+
 #define FANOUT 100
 
 struct entry
@@ -85,7 +75,7 @@ void insert(int key, int value){
 };
 
 
-nodePosition find(int key){
+struct nodePosition find(int key){
     //create a pointer to the root node
     struct node *b;
     b =  &root;
@@ -107,19 +97,9 @@ nodePosition find(int key){
             return bdata;
         }
 };
-// TODO: here you will need to define a B+Tree node(s) struct(s)
 
 
-/* The following are methods that can be invoked on B+Tree node(s).
- * Hint: You may want to review different design patterns for passing structs into C functions.
- */
 
-/* FIND (Chapter 10.4)
-This is an equality search for an entry whose key matches the target key exactly.
-How many nodes need to be accessed during an equality search for a key, within the B+Tree? 
-*/
-
-// TODO: here you will need to define FIND/SEARCH related method(s) of finding key-values in your B+Tree.
 
 
 /* INSERT (Chapter 10.5)
@@ -136,19 +116,6 @@ Bulk Load is a special operation to build a B+Tree from scratch, from the bottom
 Why might you use Bulk Load instead of a series of inserts for populating a B+Tree? Compare the cost of a Bulk Load of N data entries versus that of an insertion of N data entries? What are the tradeoffs?
 */
 
-// TODO: here you will need to define BULK LOAD related method(s) of initially adding all at once some key-values to your B+Tree.
-// BULK LOAD only can happen at the start of a workload
-
-
-/*RANGE (GRADUATE CREDIT)
-Scans are range searches for entries whose keys fall between a low key and high key.
-Consider how many nodes need to be accessed during a range search for keys, within the B+Tree?
-Can you describe two different methods to return the qualifying keys for a range search? 
-(Hint: how does the algorithm of a range search compare to an equality search? What are their similarities, what is different?)
-Can you describe a generic cost expression for Scan, measured in number of random accesses, with respect to the depth of the tree?
-*/
-
-// TODO GRADUATE: here you will need to define RANGE for finding qualifying keys and values that fall in a key range.
 
 
 
